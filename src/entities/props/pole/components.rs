ad8a5ab91008caadca04a80c718de20f2251a467
Julien Andreu <julienandreu@me.com>
2024-08-17 11:27:47 -0400

    feat: Add animation for Pole

--- src/entities/props/pole/components.rs
+++ src/entities/props/pole/components.rs
@@ -1,8 +1,22 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+use crate::animation::components::{AnimationIndices, AnimationTimer};
+
 #[derive(Default, Component)]
 pub struct Pole;
+
+impl From<&Pole> for AnimationIndices {
+    fn from(_val: &Pole) -> Self {
+        AnimationIndices { first: 0, last: 3 }
+    }
+}
+
+impl From<&Pole> for AnimationTimer {
+    fn from(_val: &Pole) -> Self {
+        AnimationTimer(Timer::from_seconds(0.128, TimerMode::Repeating))
+    }
+}
 
 #[derive(Default, Bundle, LdtkEntity)]
 pub struct PoleBundle {
--- src/entities/props/pole/systems.rs
+++ src/entities/props/pole/systems.rs
@@ -4,18 +4,16 @@ use crate::animation::components::{AnimationIndices, AnimationTimer};
 
 use super::components::*;
 
-const ANIMATION_FPS: f32 = 8.;
-
 pub fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
     query: Query<
-        (Entity, &Transform),
-        (With<Pole>, Without<AnimationIndices>),
+        (Entity, &Pole, &Transform),
+        Without<AnimationIndices>,
     >,
 ) {
-    if let Ok((e, t)) = query.get_single() {
+    if let Ok((e, p, t)) = query.get_single() {
         info!("Setup Pole animation on {:?}", e);
         let texture = asset_server.load("sprites/too-bearded/pole.png");
         let layout = TextureAtlasLayout::from_grid(
@@ -26,7 +24,7 @@ pub fn setup(
             None,
         );
         let texture_atlas_layout = texture_atlas_layouts.add(layout);
-        let animation_indices = AnimationIndices { first: 0, last: 3 };
+        let animation_indices = AnimationIndices::from(p);
         commands.entity(e).despawn_descendants().insert((
             Name::new("Pole"),
             SpriteBundle {
@@ -39,10 +37,7 @@ pub fn setup(
                 index: animation_indices.first,
             },
             animation_indices,
-            AnimationTimer(Timer::from_seconds(
-                1. / ANIMATION_FPS,
-                TimerMode::Repeating,
-            )),
+            AnimationTimer::from(p),
         ));
     }
 }
